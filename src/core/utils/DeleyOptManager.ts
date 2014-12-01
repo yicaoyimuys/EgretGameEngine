13154d48ac3a2c0edb60d185fbeccad52f03d5e3
eperfect <286844968@qq.com>
2014-12-01 15:58:12 +0800

    提交几个工具类
    热点图片，分帧处理

---
+++ src/core/utils/DeleyOptManager.ts
@@ -0,0 +1,50 @@
+/**
+ * Created by Saco on 2014/8/2.
+ */
+class DeleyOptManager{
+    private TIME_THRESHOLD:number = 3;        //每帧运算逻辑的时间阈值，执行代码超过这个时间就跳过到下一帧继续执行，根据实际情况调整，因为每一帧除了这里的逻辑还有别的逻辑要做对吧
+    private _delayOpts:any[];
+    private static _instance:DeleyOptManager;
+    public constructor()
+    {
+        this._delayOpts = [];
+    }
+
+    public static i():DeleyOptManager
+    {
+        if(!this._instance)
+            this._instance = new DeleyOptManager();
+        return this._instance;
+    }
+
+    public addDeleyOptFuncition(thisObj:any, fun:Function, funPara?:any, callBack?:Function, para?:any):void
+    {
+        this._delayOpts.push({"fun":fun, "funPara":funPara, "thisObj":thisObj, "callBack":callBack, "para":para});
+        egret.Ticker.getInstance().register(this.runCachedFun, this);
+    }
+
+    private runCachedFun(f:any):void
+    {
+        if(!this._delayOpts.length)
+            egret.Ticker.getInstance().unregister(this.runCachedFun, this);
+        var timeFlag = egret.getTimer();
+        var funObj:any;
+        while(this._delayOpts.length)
+        {
+            funObj = this._delayOpts.shift();
+            if(funObj.funPara)
+                funObj.fun.call(funObj.thisObj, [funObj.funPara]);
+            else
+                funObj.fun.call(funObj.thisObj);
+            if(funObj.callBack)
+            {
+                if(funObj.para)
+                    funObj.callBack.call(funObj.thisObj, [funObj.para]);
+                else
+                    funObj.callBack();
+            }
+            if(egret.getTimer() - timeFlag > this.TIME_THRESHOLD)
+                break;
+        }
+    }
+}
