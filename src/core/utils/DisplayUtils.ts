6e94968363e736412e862e6e889aad97cb84154e
yangsong <4707384@qq.com>
2014-12-08 13:56:44 +0800

    添加md5类

--- src/core/utils/DisplayUtils.ts
+++ src/core/utils/DisplayUtils.ts
@@ -11,6 +11,18 @@ class DisplayUtils{
     }
 
     /**
+     * 创建一个Bitmap
+     * @param resName resource.json中配置的name
+     * @returns {egret.Bitmap}
+     */
+    public createBitmap(resName:string):egret.Bitmap {
+        var result:egret.Bitmap = new egret.Bitmap();
+        var texture:egret.Texture = RES.getRes(resName);
+        result.texture = texture;
+        return result;
+    }
+
+    /**
      * 从父级移除child
      * @param child
      */
