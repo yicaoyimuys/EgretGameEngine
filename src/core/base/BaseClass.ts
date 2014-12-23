9eb2952ea191438e468ddd83820e3e3425b5dc3a
yangsong <4707384@qq.com>
2014-12-23 11:12:25 +0800

    BaseClass

--- src/core/base/BaseClass.ts
+++ src/core/base/BaseClass.ts
@@ -7,10 +7,14 @@ class BaseClass{
 
     }
 
-    public static getInstance():any{
+    /**
+     * 获取一个单例
+     * @returns {any}
+     */
+    public static getInstance(...args:any[]):any{
         var Class:any = this;
         if(!Class._instance){
-            Class._instance = new Class();
+            Class._instance = Class.create.apply(Class, arguments);
         }
         return Class._instance;
     }
