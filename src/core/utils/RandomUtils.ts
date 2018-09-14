87e115415fb7049e166946b54f0662053e46d50d
yangsong <yangsong@egret.com>
2018-09-14 14:51:25 +0800

    修复随机获取区间内整数分布不均匀的问题

--- src/core/utils/RandomUtils.ts
+++ src/core/utils/RandomUtils.ts
@@ -8,21 +8,21 @@ class RandomUtils extends BaseClass {
      * @param $end 最大值
      * @returns {number}
      */
-    public limit($from:number, $end:number):number {
+    public limit($from: number, $end: number): number {
         $from = Math.min($from, $end);
         $end = Math.max($from, $end);
-        var range:number = $end - $from;
+        var range: number = $end - $from;
         return $from + Math.random() * range;
     }
 
     /**
-     * 获取一个区间的随机数(帧数)
+     * 获取一个区间的随机数(整数)
      * @param $from 最小值
      * @param $end 最大值
      * @returns {number}
      */
-    public limitInteger($from:number, $end:number):number {
-        return Math.round(this.limit($from, $end));
+    public limitInteger($from: number, $end: number): number {
+        return Math.floor(this.limit($from, $end + 1));
     }
 
     /**
@@ -30,8 +30,8 @@ class RandomUtils extends BaseClass {
      * @param arr 数组
      * @returns {any} 随机出来的结果
      */
-    public randomArray(arr:Array<any>):any {
-        var index:number = Math.floor(Math.random() * arr.length);
+    public randomArray(arr: Array<any>): any {
+        var index: number = Math.floor(Math.random() * arr.length);
         return arr[index];
     }
 }
