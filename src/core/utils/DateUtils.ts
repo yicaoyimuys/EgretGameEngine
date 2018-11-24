3c8d7363a38c128751bc961055acaa9be4d458e7
yangsong <4607384@qq.com>
2018-11-24 15:27:31 +0800

    添加日期格式化

--- src/core/utils/DateUtils.ts
+++ src/core/utils/DateUtils.ts
@@ -232,4 +232,26 @@ class DateUtils extends BaseClass {
             && date1.getMonth() == date2.getMonth()
             && date1.getDate() == date2.getDate();
     }
+
+    /**
+     * 日期格式化
+     */
+    public format(d: Date, fmt: string = "yyyy-MM-dd hh:mm:ss"): string {
+        let o = {
+            "M+": d.getMonth() + 1, //month
+            "d+": d.getDate(),    //day
+            "h+": d.getHours(),   //hour
+            "m+": d.getMinutes(), //minute
+            "s+": d.getSeconds(), //second
+            "q+": Math.floor((d.getMonth() + 3) / 3),  //quarter
+            "S": d.getMilliseconds() //millisecond
+        }
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1,
+            (d.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o) if (new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1,
+                RegExp.$1.length == 1 ? o[k] :
+                    ("00" + o[k]).substr(("" + o[k]).length));
+        return fmt;
+    }
 }
